b73aebe7546a5fb9588361457dad34bfcef5ce91
allisonkydy <allisnelly@gmail.com>
2019-08-14 15:28:16 -0500

    add nasa logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import logo from '../assets/nasa-logo.png';
+
 function Header() {
    return (
       <div className="header">
-         <img src='' alt='' />
+         <img src={logo} alt='NASA logo' />
          <h1>Astronomy Picture of the Day</h1>
       </div>
    );
