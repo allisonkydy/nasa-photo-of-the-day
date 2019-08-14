af2f8f4d762ba7cbee66e317644f8fda5657e198
allisonkydy <allisnelly@gmail.com>
2019-08-14 16:09:56 -0500

    factor out Date into new component

---
+++ src/components/Date.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+function Date(props) {
+   if (!props.date) return <h3>Loading...</h3>;
+
+   const formatDate = () => {
+      const date = props.date;
+      const day = date.slice(8);
+      const month = date.slice(5, 7);
+      const year = date.slice(0, 4);
+      let newDate = [];
+
+      switch(month) {
+         case '01':
+            newDate.push('January ');
+            break;
+         case '02':
+            newDate.push('February ');
+            break;
+         case '03':
+            newDate.push('March ');
+            break;
+         case '04':
+            newDate.push('April ');
+            break; 
+         case '05':
+            newDate.push('May ');
+            break;
+         case '06':
+            newDate.push('June ');
+            break;
+         case '07':
+            newDate.push('July ');
+            break;
+         case '08':
+            newDate.push('August ');
+            break; 
+         case '09':
+            newDate.push('September ');
+            break;
+         case '10':
+            newDate.push('October ');
+            break;
+         case '11':
+            newDate.push('November ');
+            break;
+         case '12':
+            newDate.push('December ');
+            break;       
+      }
+      
+      newDate.push(`${day}, `);
+      newDate.push(year);
+
+      return newDate.join('');
+   }
+
+   return (
+      <>
+         <h3>{formatDate(props.date)}</h3>
+      </>
+   )
+}
+
+export default Date;
