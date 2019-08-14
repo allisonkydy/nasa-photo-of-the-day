62c44f2b0c554b5496fcf9b9c6f6e71d4611dbfe
allisonkydy <allisnelly@gmail.com>
2019-08-14 15:15:47 -0500

    set state and axios get request

--- src/App.js
+++ src/App.js
@@ -7,14 +7,19 @@ import Description from "./components/Description";
 
 // https://api.nasa.gov/planetary/apod?api_key=D8oIlefuvBfmqihhqjyaU4JKZTqn5qWEXEf6jPmU
 function App() {
+  const [imgURL, setImgURL] = useState('');
+  const [date, setDate] = useState();
+  const [title, setTitle] = useState();
+  const [expl, setExpl] = useState();
 
-  // useEffect(() => {
-  //   axios.get('https://api.nasa.gov/planetary/apod?api_key=D8oIlefuvBfmqihhqjyaU4JKZTqn5qWEXEf6jPmU')
-  //     .then(response => {
-  //       console.log(response);
-  //     })
-  //     .catch(err => console.log(err))
-  // }, [])
+
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=D8oIlefuvBfmqihhqjyaU4JKZTqn5qWEXEf6jPmU')
+      .then(response => {
+        console.log(response);
+      })
+      .catch(err => console.log(err))
+  }, [])
 
   return (
     <div className="App">
