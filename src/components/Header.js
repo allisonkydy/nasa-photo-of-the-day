8e1379d4a9534bbf42587bae941bf3ee24d2349a
allisonkydy <allisnelly@gmail.com>
2019-08-15 16:52:14 -0500

    delete boilerplate css, add styles to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,29 @@
 import React from 'react';
-import { Container, Header, Image } from 'semantic-ui-react';
+import styled from 'styled-components';
 
 import logo from '../assets/nasa-logo.png';
 
 function ApodHeader() {
+   const Container = styled.div`
+      margin: 20px auto 30px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+   `
+
+   const Logo = styled.img`
+      transform: scale(0.7);
+   `
+
+   const Header = styled.h1`
+      text-align: center;      
+   `
+
    return (
-      <div className="header">
-         <img src={logo} alt='NASA logo' />
-         <h1>Astronomy Picture of the Day</h1>
-      </div>
+      <Container>
+         <Logo src={logo} alt='NASA logo' />
+         <Header>Astronomy Picture of the Day</Header>
+      </Container>
    );
 }
 
